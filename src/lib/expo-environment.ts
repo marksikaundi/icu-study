8c135e4031b553da1dec97672df18791c0c2e232
marksikaundi <emmanuelmark117@gmail.com>
2026-05-08 03:17:56 +0200

    chore: remove deprecated files and configurations, including environment settings, project structure, and unused assets

--- src/lib/expo-environment.ts
+++
@@ -1,4 +0,0 @@
-import Constants from "expo-constants";
-
-/** Expo Go cannot use remote push (SDK 53+ on Android; limited on iOS). Use a dev build instead. */
-export const isExpoGo = () => Constants.appOwnership === "expo";
